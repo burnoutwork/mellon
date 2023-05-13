ef806c9c70b561aea35cfee02b7ce521402c4943
burnout <burnout@mail.com>
2023-05-13 16:00:04 +0300

    Change space jobs

--- .space.kts
+++ .space.kts
@@ -1,3 +1,11 @@
 job("Test contract") {
-    container(displayName = "Run contract tests", image = "melonweb3/melon:0.1")
+    container(displayName = "Run contract tests", image = "melonweb3/melon:0.1") {
+        shellScript {
+            content = """
+                set -e
+                echo Run tests
+                npm run test
+            """
+        }
+    }
 }
