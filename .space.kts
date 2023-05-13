0901fc20a510ad805969bcfbb53011a214227421
sagruvi <nedelkoaroslav@gmail.com>
2023-05-13 14:35:41 +0300

    fix .space.kts

--- .space.kts
+++ .space.kts
@@ -2,8 +2,7 @@ job("run tests") {
 	container(displayName = "test_contract", image = "ubuntu") {
 		shellScript {
 			interpreter = "/bin/bash"
-			content = "apt-get update && apt-get install npm rustc cargo" +
-					  "npm run test"
+			content = "apt-get update && apt-get install npm rustc cargo && npm run test"
 		}
 	}
 }
