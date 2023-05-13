8c3a252e4cdadad4f1aafdf25d3ea2e16284a2e8
burnout <burnout@mail.com>
2023-05-13 15:54:24 +0300

    Add Dockerfile

--- .space.kts
+++ .space.kts
@@ -1,13 +1,3 @@
 job("Test contract") {
-    container(displayName = "Run contract tests", image = "rustlang/rust:nightly") {
-        shellScript {
-            content = """
-                set -e
-                # Build install dependents
-                npm run init:contract
-                # Run tests
-                npm run test
-            """
-        }
-    }
+    container(displayName = "Run contract tests", image = "melonweb3/melon:0.1")
 }
